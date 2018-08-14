a0e220e31d853108aaed6520504e3c4c73780802
Denis Beliaev <cimmwolf@gmail.com>
2018-08-14 20:52:12 +0300

    Update SiteController.php

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,6 +9,7 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use DenisBeliaev\AdaptiveImg;
 
 class SiteController extends Controller
 {
